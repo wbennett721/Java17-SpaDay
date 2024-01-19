b06e877b54c32d72d403a3c139ced7f08cbc4868
wbennett721 <142439301+wbennett721@users.noreply.github.com>
2024-01-18 22:22:39 -0600

    StudioSpaDay

--- src/main/java/org/launchcode/controllers/SpaDayController.java
+++ src/main/java/org/launchcode/controllers/SpaDayController.java
@@ -62,6 +62,14 @@ public class SpaDayController {
                 appropriateFacials.add(facials.get(i));
             }
         }
+        model.addAttribute("appropriateFacials", appropriateFacials);
+
+        model.addAttribute("name", name);
+        model.addAttribute("skintype", skintype);
+        model.addAttribute("manipedi", manipedi);
+
+
+
 
         return "menu";
     }
