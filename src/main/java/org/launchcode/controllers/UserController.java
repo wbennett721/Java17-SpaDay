3d90bdfef24f088d57c632574365961875af550b
wbennett721 <142439301+wbennett721@users.noreply.github.com>
2024-01-25 23:03:57 -0600

    StudioCh15

--- src/main/java/org/launchcode/controllers/UserController.java
+++ src/main/java/org/launchcode/controllers/UserController.java
@@ -1,8 +1,10 @@
 package org.launchcode.controllers;
 
+import jakarta.validation.Valid;
 import org.launchcode.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
@@ -10,20 +12,16 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
 
     @GetMapping("/add")
-    public String displayAddUserForm() {
+    public String displayAddUserForm(Model model) {
+        model.addAttribute(new User());
         return "user/add";
     }
 
     @PostMapping
-    public String processAddUserForm(Model model, @ModelAttribute User user, String verify) {
-        model.addAttribute("user", user);
-        model.addAttribute("verify", verify);
-        model.addAttribute("username", user.getUsername());
-        model.addAttribute("email", user.getEmail());
-        if (user.getPassword().equals(verify)) {
+    public String processAddUserForm(Model model, @ModelAttribute @Valid User user, Errors errors, String verify) {
+        if (!errors.hasErrors() && user.getPassword().equals(verify)) {
             return "user/index";
-        }
-        else {
+        } else {
             model.addAttribute("error", "Passwords do not match");
             return "user/add";
         }
--- src/main/java/org/launchcode/models/User.java
+++ src/main/java/org/launchcode/models/User.java
@@ -1,8 +1,14 @@
 package org.launchcode.models;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
+
 public class User {
+    @Size(min = 5, max = 15, message = "Username must be 5-15 characters long.")
     private String username;
+    @Email(message = "Invalid email format.")
     private String email;
+    @Size(min = 6, message = "Password must be at least 6 characters long.")
     private String password;
 
     public User() {
